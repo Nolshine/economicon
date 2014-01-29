4e993366156c23dec68d93a04d715e40fabe8754
Ariel M Shaked <mtr131@gmail.com>
2014-01-29 23:12:27 +0000

    added Tile class for renderer and generator to work with

---
+++ entities.py
@@ -0,0 +1,50 @@
+if __name__ == "__main__":
+    import pygame
+    from pygame.locals import *
+
+
+class Tile:
+    #this is global data, accessible by all instances
+    spritesheet = []
+    print "loading dirt sprites..."
+    #here I load dirt sprites and scale them x2 at the same time
+    spritesheet.append(pygame.transform.scale(
+        pygame.image.load("dirt.png"), (32, 32)))
+    spritesheet.append(pygame.transform.scale(
+        pygame.image.load("dirt_back.png"), (32, 32)))
+    print "done loading dirt sprites."
+
+    def __init__(self, is_dug = 1):
+        self.is_dug = is_dug
+        self.sprite = Tile.spritesheet[is_dug]
+
+
+if __name__ == "__main__":
+    tile1 = Tile()
+    tile2 = Tile(0)
+
+    pygame.init()
+
+    screen = pygame.display.set_mode((64, 64), DOUBLEBUF)
+
+    clock = pygame.time.Clock()
+
+    #test loop
+    try:
+        print "Rendering dug (empty) dirt Tile..."
+        for i in range(400):
+            clock.tick(60)
+            pygame.event.pump()
+            screen.blit(tile1.sprite, (16, 16))
+            pygame.display.update()
+        print "Done. Rendering undug (full) dirt Tile./."
+        for i in range(400):
+            clock.tick(60)
+            pygame.event.pump()
+            screen.blit(tile2.sprite, (16, 16))
+            pygame.display.update()
+        pygame.quit()
+    except:
+        print "exception quit..."
+        pygame.quit()
+            
